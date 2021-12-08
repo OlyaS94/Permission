ddaea2fa803d93e65001e070153af8dfd58b9990
Olga Smirnova <olya_fcsm@mail.ru>
2021-12-08 20:34:18 +0300

    first

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,27 @@
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from .filters import AdvertisementFilter
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", 'destroy', ]:
+            return [permissions.IsAuthenticated(), IsOwnerOrReadOnly(), ]
+        return []
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.creator == request.user
